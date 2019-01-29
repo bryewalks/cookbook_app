5ac96400444f26f2a52205472145ba002b8510f8
bryewalks <bryewalks@gmail.com>
2019-01-29 11:10:31 -0600

    encode decode

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -25,6 +25,8 @@ class Api::RecipesController < ApplicationController
   end
 
   def show
+    @current_user = current_user
+
     @recipe = Recipe.find(params[:id])
     render 'show.json.jbuilder'
   end
