a33f0ed4178421763bb7ccb44f36018e99c2cf93
bryewalks <bryewalks@gmail.com>
2019-01-29 12:23:58 -0600

    small changes before end of day

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -25,8 +25,6 @@ class Api::RecipesController < ApplicationController
   end
 
   def show
-    @current_user = current_user
-
     @recipe = Recipe.find(params[:id])
     render 'show.json.jbuilder'
   end
--- app/views/api/recipes/show.json.jbuilder
+++ app/views/api/recipes/show.json.jbuilder
@@ -1,3 +1,5 @@
-json.current_user @current_user
+if current_user
+  json.current_user current_user.email
+end
 
 json.partial! @recipe, partial: "recipe", as: :recipe
